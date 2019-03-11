e6eb3dd1af5c9c6a5174ec450ab2050f2801913b
Dawid Kamasz <dawid.kamasz@gmail.com>
2019-03-11 20:17:29 +0100

    FX-sprawdz pogode na gotowo

--- src/main/java/pl/sda/Controller/RootController.java
+++ src/main/java/pl/sda/Controller/RootController.java
@@ -1,10 +1,13 @@
 package pl.sda.Controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import pl.sda.Pogoda.Pogoda;
+import pl.sda.Pogoda.SerwisPogody;
 
 //import java.awt.*;
 import java.net.URL;
@@ -17,13 +20,23 @@ public class RootController implements Initializable {
     private TextField city;
     @FXML
     private Label TemperatureIn;
+    @FXML
+    private Label miasto;
+    @FXML
+    private Label temp;
+    @FXML
+    private Label czas;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        city.setText("Czy wypijesz w piatek wodeczke z Przemasem?");
-        TemperatureIn.setText("");
+        city.setText("");
+        TemperatureIn.setText("W jakim miescie chcialbys sprawdzic pogode?");
     }
     public void setCity(){
-        System.out.println("proste!");
+        SerwisPogody serwisPogody= new SerwisPogody();
+        Pogoda pogoda= serwisPogody.getCityPogoda(city.getText());
+        miasto.setText(pogoda.getLocation().getName());
+        temp.setText(String.valueOf(pogoda.getCurrent().getTemp_c())+" stopnie");
+        czas.setText(pogoda.getLocation().getLocaltime());
     }
 }
